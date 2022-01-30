045283ec2c848dac5e9c01178d68f4291529a6d1
Muhammad Zeeshan <77879378+zexhan17@users.noreply.github.com>
2022-01-30 11:26:48 +0500

    Add files via upload

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+	import Footer from '$lib/components/footer.svelte';
+	import Navbar from '$lib/components/navbar.svelte';
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+
+	onMount(async () => {
+		themeChange(false)
+	})
+</script>
+
+<Navbar />
+<main class="container max-w-3xl mx-auto px-4 mb-20">
+	<slot />
+</main>
+<Footer />
